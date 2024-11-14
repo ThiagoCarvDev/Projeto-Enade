6cf50914455e555e7d1a6eeb2ba00f1d93a96506
Aparecida-Lima19 <ci.lima.silva22@gmail.com>
2024-11-14 20:22:18 -0300

    Novo servidor

--- src/javascript/ajax.js
+++ src/javascript/ajax.js
@@ -2,7 +2,7 @@
 class Ajax
 {
   /**@readonly @type {string} URL mãe do projeto*/
-  static #URLbase = "http://3.94.87.76:8080/api/";
+  static #URLbase = "http://54.165.34.4:8080/api/";
 
 
   /**Função que desemcripta o token e retorna seu valor.
