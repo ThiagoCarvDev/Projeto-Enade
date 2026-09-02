3c3dec45d358ccdead9f9e108860e1e430c622eb
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-01 23:44:22 -0300

    feat: implementa tratamendo de exceção para acessos Denied no WebSecurityConfig

--- enade-back/src/main/java/com/ads/enade/security/WebSecurityConfig.java
+++ enade-back/src/main/java/com/ads/enade/security/WebSecurityConfig.java
@@ -1,12 +1,13 @@
 package com.ads.enade.security;
 
+import com.ads.enade.security.jwt.AuthDeniedHandlerJwt;
 import com.ads.enade.security.jwt.AuthEntryPointJwt;
 import com.ads.enade.security.jwt.AuthTokenFilter;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
@@ -19,8 +20,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.List;
 
@@ -30,28 +31,28 @@ import java.util.List;
  */
 @Configuration
 @EnableMethodSecurity // Anotação para habilitar a segurança em métodos
-@SecurityScheme(name = WebSecurityConfig.SECURITY, type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+@RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    public static final String SECURITY = "bearerSecurity";
-
-    @Autowired
-    private AuthEntryPointJwt unauthorizedHandler;
-
-    @Autowired
-    private UserDetailsServiceImpl userDetailsService;
+    private final AuthEntryPointJwt unauthorizedHandler;
+    private final AuthDeniedHandlerJwt accessDeniedHandler;
+    private final UserDetailsServiceImpl userDetailsService;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // Método para criar uma cadeia de filtros de segurança
         http.csrf(AbstractHttpConfigurer::disable) // Desativa a proteção contra ataques CSRF (PROCURAR MELHORIAS)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
-                .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler)) // Configura o manipulador de entrada não autorizada
+                .exceptionHandling(exception -> exception
+                        .authenticationEntryPoint(unauthorizedHandler)
+                        .accessDeniedHandler(accessDeniedHandler)
+                ) // Configura o manipulador de entrada não autorizada
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))  // Configura a política de criação de sessão como sem estado
                 .headers(hearder -> hearder.frameOptions(frame -> frame.sameOrigin())) //Libera o acesso ao H2
                 .authorizeHttpRequests(auth -> // Configura as autorizações de solicitações HTTP
                         auth.requestMatchers("/api/auth/**").permitAll()
                                 .requestMatchers("/api/test/**").permitAll()
                                 .requestMatchers("/swagger-ui.html","/swagger-ui/**", "/v3/api-docs/**", "/h2-console/**").permitAll() // Libera rotas do swagger e h2 (apenas para teste)
+                                .requestMatchers(HttpMethod.POST,"/api/questao").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 );
 
