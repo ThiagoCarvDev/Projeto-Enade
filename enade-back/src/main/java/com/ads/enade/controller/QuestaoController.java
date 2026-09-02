a1e2248c76072a1856ff1400a4b73c097ed42f07
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-01 23:52:35 -0300

    refactor: atualiza o Controller de QuestaoController para retornar DTO

--- enade-back/src/main/java/com/ads/enade/controller/QuestaoController.java
+++ enade-back/src/main/java/com/ads/enade/controller/QuestaoController.java
@@ -6,10 +6,8 @@ import com.ads.enade.service.QuestaoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/questao")
@@ -18,11 +16,21 @@ public class QuestaoController {
 
     private final QuestaoService questaoService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<Void> registerQuestion(@RequestBody QuestaoDTORequest request){
+    public ResponseEntity<QuestaoDTOResponse> registerQuestion(@RequestBody QuestaoDTORequest request){
 
-        questaoService.registerQuestion(request);
+        QuestaoDTOResponse response = questaoService.registerQuestion(request);
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<QuestaoDTOResponse> findById(@PathVariable Long id){
+
+        QuestaoDTOResponse response = questaoService.findById(id);
+
+        return ResponseEntity.ok(response);
     }
 }
