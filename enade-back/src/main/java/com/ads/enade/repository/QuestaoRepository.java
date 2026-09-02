32605705bea7dea459f5ee72177cc1a5c4959d2e
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-01 23:48:41 -0300

    feat: cria repositories de CursoRepository e SimuladoRepository, e cria query de buscar questões no QuestaoRepository

--- enade-back/src/main/java/com/ads/enade/repository/QuestaoRepository.java
+++ enade-back/src/main/java/com/ads/enade/repository/QuestaoRepository.java
@@ -2,9 +2,15 @@ package com.ads.enade.repository;
 
 import com.ads.enade.entity.Questao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestaoRepository extends JpaRepository<Questao, Long> {
 
+    @Query(value = "SELECT * FROM questao ORDER BY RAND() LIMIT :quantidade", nativeQuery = true)
+    List<Questao> buscarDezQuestoes(@Param("quantidade") int quantidade);
 }
