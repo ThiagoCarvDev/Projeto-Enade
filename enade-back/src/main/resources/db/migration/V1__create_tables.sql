bd46832e6072acf82ba7fa03b4e3d373cb907fb0
Pedro Nunes <pedrovitornunes89@gmail.com>
2026-09-01 23:45:42 -0300

    feat: adiciona novas entidade no sql do V1 do flyway

--- enade-back/src/main/resources/db/migration/V1__create_tables.sql
+++ enade-back/src/main/resources/db/migration/V1__create_tables.sql
@@ -1,4 +1,4 @@
--- Tabela de Cursos (courses)
+-- Tabela de Cursos
 CREATE TABLE curso
 (
     id   BIGINT AUTO_INCREMENT,
@@ -16,20 +16,18 @@ CREATE TABLE roles
     CONSTRAINT uc_roles_name UNIQUE (name)
 );
 
--- Tabela de Usuários (usuarios)
-CREATE TABLE usuarios
+-- Tabela de Usuários
+CREATE TABLE usuario
 (
     id            BIGINT AUTO_INCREMENT,
     username      VARCHAR(20)  NOT NULL,
     email         VARCHAR(50)  NOT NULL,
     password      VARCHAR(120) NOT NULL,
-    course_id     BIGINT,
-    score         INT DEFAULT 0,
-    quiz_attempts INT DEFAULT 0,
+    id_curso     BIGINT,
     PRIMARY KEY (id),
     CONSTRAINT uc_users_username UNIQUE (username),
     CONSTRAINT uc_users_email UNIQUE (email),
-    CONSTRAINT fk_users_course FOREIGN KEY (course_id) REFERENCES curso (id)
+    CONSTRAINT fk_users_course FOREIGN KEY (id_curso) REFERENCES curso (id)
 );
 
 -- Tabela de Tokens de Redefinição de Senha (password_reset_token)
@@ -41,7 +39,7 @@ CREATE TABLE password_reset_token
     expiry_date DATETIME(6),
     PRIMARY KEY (id),
     CONSTRAINT uc_password_reset_token_user UNIQUE (user_id),
-    CONSTRAINT fk_password_reset_token_user FOREIGN KEY (user_id) REFERENCES usuarios (id)
+    CONSTRAINT fk_password_reset_token_user FOREIGN KEY (user_id) REFERENCES usuario (id)
 );
 
 -- Tabela de Associação Usuários e Papéis (user_roles)
@@ -50,7 +48,7 @@ CREATE TABLE user_roles
     user_id BIGINT NOT NULL,
     role_id BIGINT NOT NULL,
     PRIMARY KEY (user_id, role_id),
-    CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES usuarios (id),
+    CONSTRAINT fk_user_roles_user FOREIGN KEY (user_id) REFERENCES usuario (id),
     CONSTRAINT fk_user_roles_role FOREIGN KEY (role_id) REFERENCES roles (id)
 );
 
@@ -59,11 +57,11 @@ CREATE TABLE questao
 (
     id_questao    INT AUTO_INCREMENT PRIMARY KEY,
     titulo        VARCHAR(150)  NOT NULL,
-    enunciado     VARCHAR(1000) NOT NULL,
+    enunciado     VARCHAR(2000) NOT NULL,
     tipo_questao  VARCHAR(100)  NOT NULL,
     area_questao  VARCHAR(100)  NOT NULL,
     possui_imagem BOOLEAN       NOT NULL,
-    explicacao    VARCHAR(250),
+    explicacao    VARCHAR(1000),
     img_url       VARCHAR(255)
 );
 
@@ -76,4 +74,46 @@ CREATE TABLE alternativa
     opcao_alternativa VARCHAR(10),
     id_questao        INT NOT NULL,
     CONSTRAINT fk_alternativa_questao FOREIGN KEY (id_questao) REFERENCES questao (id_questao)
+);
+
+CREATE TABLE IF NOT EXISTS simulado (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    data_criacao DATE NOT NULL,
+    id_curso BIGINT,
+    tipo_simulado ENUM('ORIGINAL', 'SIMULADO') NOT NULL,
+    CONSTRAINT fk_simulado_curso FOREIGN KEY (id_curso) REFERENCES curso(id)
+);
+
+CREATE TABLE IF NOT EXISTS simulado_questao (
+    id_simulado BIGINT NOT NULL,
+    id_questao BIGINT NOT NULL,
+    PRIMARY KEY (id_simulado, id_questao),
+    CONSTRAINT fk_simuladoquestao_simulado FOREIGN KEY (id_simulado) REFERENCES simulado(id),
+    CONSTRAINT fk_simuladoquestao_questao FOREIGN KEY (id_questao) REFERENCES questao(id_questao)
+);
+
+
+CREATE TABLE IF NOT EXISTS usuario_simulado (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id_usuario BIGINT,
+    id_simulado BIGINT,
+    nota DOUBLE,
+    quantidade_acertos INT,
+    data_conclusao DATE,
+    CONSTRAINT fk_usuariosimulado_usuario FOREIGN KEY (id_usuario) REFERENCES usuario(id),
+    CONSTRAINT fk_usuariosimulado_simulado FOREIGN KEY (id_simulado) REFERENCES simulado(id)
+);
+
+
+CREATE TABLE IF NOT EXISTS usuario_questao (
+   id BIGINT AUTO_INCREMENT PRIMARY KEY,
+   id_usuario_simulado BIGINT,
+   id_questao BIGINT,
+   is_acerto BOOLEAN NOT NULL,
+   data_resposta DATE NOT NULL,
+   id_alternativa BIGINT,
+   CONSTRAINT fk_usuarioquestao_usuariosimulado FOREIGN KEY (id_usuario_simulado) REFERENCES usuario_simulado(id),
+    CONSTRAINT fk_usuarioquestao_questao FOREIGN KEY (id_questao) REFERENCES questao(id_questao),
+    CONSTRAINT fk_usuarioquestao_alternativa FOREIGN KEY (id_alternativa) REFERENCES alternativa(id_alternativa)
 );
